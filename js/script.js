87f0498c29075f8ba89cf0d266989c57a15c9215
First Last <me@example.com>
2026-07-22 15:47:53 +0300

    responsibility done

--- js/script.js
+++ js/script.js
@@ -20,7 +20,32 @@ if (typeof jQuery.isNumeric !== 'function') {
     }).on('circle-animation-progress', function(event, progress, stepValue){
       $(this).find('span').html(Math.round(stepValue * 100) + '%');
     });
+    $(function() {
+      $('#cotactForm').validate()
+    })
   });
 
 
   document.getElementById('currentyear').innerHTML= new Date().getFullYear();
+
+
+
+  // Example starter JavaScript for disabling form submissions if there are invalid fields
+(() => {
+  'use strict'
+
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  const forms = document.querySelectorAll('.needs-validation')
+
+  // Loop over them and prevent submission
+  Array.from(forms).forEach(form => {
+    form.addEventListener('submit', event => {
+      if (!form.checkValidity()) {
+        event.preventDefault()
+        event.stopPropagation()
+      }
+
+      form.classList.add('was-validated')
+    }, false)
+  })
+})()
